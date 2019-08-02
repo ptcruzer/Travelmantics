0c3b395058081e9b087d8ac523005a2d5092612b
Mogammad <vx1@webmail.co.za>
2019-08-02 16:40:04 +0200

    Challenge Version

---
+++ app/src/main/java/com/example/travelmantics/DealAdapter.java
@@ -0,0 +1,128 @@
+package com.example.travelmantics;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class DealAdapter extends RecyclerView.Adapter<DealAdapter.DealViewHoler> {
+
+    ArrayList<TravelDeal> deals;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    private ChildEventListener mChildListener;
+    private ImageView imageDeal;
+
+    public DealAdapter() {
+        //FirebaseUtil.openFbReference("traveldeals");
+        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
+        mDatabaseReference = FirebaseUtil.mDatabaseReverence;
+        this.deals = FirebaseUtil.mDeals;
+        mChildListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                TravelDeal td = dataSnapshot.getValue(TravelDeal.class);
+                Log.d("Deal: ", td.getTitle());
+                td.setId(dataSnapshot.getKey());
+                deals.add(td);
+                notifyItemInserted(deals.size() - 1);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        mDatabaseReference.addChildEventListener(mChildListener);
+    }
+
+    @NonNull
+    @Override
+    public DealViewHoler onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        View itemView = LayoutInflater.from(context).inflate(R.layout.rv_row, parent, false);
+        return new DealViewHoler(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DealViewHoler holder, int position) {
+        TravelDeal deal = deals.get(position);
+        holder.bind(deal);
+    }
+
+    @Override
+    public int getItemCount() {
+        return deals.size();
+    }
+
+    public class DealViewHoler extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView tvTitle;
+        TextView tvDescription;
+        TextView tvPrice;
+
+        public DealViewHoler(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = (TextView) itemView.findViewById(R.id.tvTitle1);
+            tvDescription = (TextView) itemView.findViewById(R.id.tvDescription1);
+            tvPrice = (TextView) itemView.findViewById(R.id.tvPrice1);
+            imageDeal = (ImageView) itemView.findViewById(R.id.imageDeal);
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(TravelDeal deal) {
+            tvTitle.setText(deal.getTitle());
+            tvDescription.setText(deal.getDescription());
+            tvPrice.setText(deal.getPrice());
+            showImage(deal.getImageUrl());
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            Log.d("Click", String.valueOf(position));
+            TravelDeal selectedDeal = deals.get(position);
+            Intent intent = new Intent(v.getContext(), DealActivity.class);
+            intent.putExtra("Deal", selectedDeal);
+            v.getContext().startActivity(intent);
+        }
+    }
+
+    private void showImage(String url) {
+        if (url != null && url.isEmpty() == false) {
+            Picasso.with(imageDeal.getContext()).load(url).resize(160,160).centerCrop().into(imageDeal);
+        }
+    }
+}
